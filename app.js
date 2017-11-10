b0c2552aa53400c6adfc6065d9b1054b1851e0fb
EC2 Default User <ec2-user@ip-172-31-50-63.ec2.internal>
2017-11-10 03:40:14 +0000

    added public key

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello jenkins code pipeline');
+  res.send('hello jenkins code pipeline on ec2 instance');
 });
  
 app.listen(process.env.PORT || 5000);
--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello jenkins code pipeline' , done);
+    request(app).get('/').expect('hello jenkins code pipeline on ec2 instance' , done);
   });
 });
